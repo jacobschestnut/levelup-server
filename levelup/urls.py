9f21ce81ff5496730095ab7db56a48ebc4e2974d
Jake Chestnut <jacobschestnut@gmail.com>
2022-06-02 10:51:34 -0500

    june 2

--- levelup/urls.py
+++ levelup/urls.py
@@ -5,14 +5,16 @@ from levelupapi.views import register_user, login_user
 from rest_framework import routers
 from levelupapi.views import GameTypeView
 from levelupapi.views import GameView
+from levelupapi.views import EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('', include(router.urls))
 ]
--- levelupapi/views/game_type.py
+++ levelupapi/views/game_type.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers, status
+from rest_framework import serializers
 from levelupapi.models import GameType
 
 
