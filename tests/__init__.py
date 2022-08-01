d275ed191614cffb8b736ca84427f16b7fed50ea
Jake Chestnut <jacobschestnut@gmail.com>
2022-08-01 16:26:36 -0500

    added test module

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# from .gamer_tests import Gamer
+# from .game_type_tests import GameType
+# from .game_tests import Game
+# from .event_tests import Event
+# from .event_gamer_tests import EventGamer
