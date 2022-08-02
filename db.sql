5076671e2fc23c8f61d48c257f62c00af791a11c
Jake Chestnut <jacobschestnut@gmail.com>
2022-08-01 19:42:47 -0500

    added templates

--- db.sql
+++ db.sql
@@ -1,1 +1,5 @@
-SELECT * FROM levelupapi_game;
+SELECT g.title, u.first_name, u.last_name, gr.id
+    FROM levelupapi_game g
+    JOIN levelupapi_gamer gr ON gr.user_id = g.gamer_id
+    JOIN auth_user u ON gr.user_id = u.id
+    WHERE g.gamer_id = gr.user_id
